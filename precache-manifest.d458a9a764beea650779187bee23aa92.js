ad72f7c90d8fc5f314b9bdfcf7a163a6806e4991
gadenahi@gmail.com <gadenahi@gmail.com>
2020-08-27 15:18:34 -0700

    Updates

---
+++ precache-manifest.d458a9a764beea650779187bee23aa92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24cc0cc5bbd2b34b5e434b4490f6bd7b",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "8136ca340e99cb174a4f",
+    "url": "/pomodoro-clock/static/css/main.bc149b74.chunk.css"
+  },
+  {
+    "revision": "3b38a05ea81abb96569a",
+    "url": "/pomodoro-clock/static/js/2.a6d7ed52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.a6d7ed52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8136ca340e99cb174a4f",
+    "url": "/pomodoro-clock/static/js/main.d0f140a1.chunk.js"
+  },
+  {
+    "revision": "d420e891230cef74bfe7",
+    "url": "/pomodoro-clock/static/js/runtime-main.cd73fa2f.js"
+  },
+  {
+    "revision": "988833bf34d92eba3d536029cd9e1bc6",
+    "url": "/pomodoro-clock/static/media/BeepSound.988833bf.wav"
+  }
+]);
